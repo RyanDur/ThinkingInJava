4e16c5ae151f52df1c4336ddd27c6e0159a0178d
RyanDurling <ryan.p.durling@gmail.com>
2012-09-03 12:25:26 -0700

    Exercise 45

---
+++ EverythingIsAnObject/src/Ex5.java
@@ -0,0 +1,13 @@
+public class Ex5 {
+    public static void main(String[] args) {
+        DataOnly data = new DataOnly();
+
+        data.i = 5;
+        data.d = 3.34;
+        data.b = true;
+
+        System.out.println(data.i);
+        System.out.println(data.d);
+        System.out.println(data.b);
+    }
+}

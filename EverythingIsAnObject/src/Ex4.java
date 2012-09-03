56e8111f21fd8c658bb41aeb635559359feb0b38
RyanDurling <ryan.p.durling@gmail.com>
2012-09-03 12:21:36 -0700

    Exercise 4

---
+++ EverythingIsAnObject/src/Ex4.java
@@ -0,0 +1,9 @@
+public class Ex4 {
+    public static void main(String[] args) {
+        DataOnly dataOnly = new DataOnly();
+
+        dataOnly.i = 47;
+        dataOnly.d = 1.1;
+        dataOnly.b = false;
+    }
+}

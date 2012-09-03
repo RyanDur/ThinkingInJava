3d01c86d28a144e6b63af950395518ba5f67882c
RyanDurling <ryan.p.durling@gmail.com>
2012-09-03 13:31:11 -0700

    Exercise 9

---
+++ EverythingIsAnObject/src/Ex9.java
@@ -0,0 +1,43 @@
+public class Ex9 {
+    public static void main(String[] args) {
+        int i = 1;
+        Integer I;
+        I = i;
+        System.out.println(I);
+
+        boolean b = true;
+        Boolean B;
+        B = b;
+        System.out.println(B);
+
+        char ch = 'c';
+        Character Ch;
+        Ch = ch;
+        System.out.println(Ch);
+
+        byte by = 3;
+        Byte By;
+        By = by;
+        System.out.println(By);
+
+        short sh = 6;
+        Short Sh;
+        Sh = sh;
+        System.out.println(Sh);
+
+        long l = 9;
+        Long L;
+        L = l;
+        System.out.println(L);
+
+        float f = (float) 1.1;
+        Float F;
+        F = f;
+        System.out.println(F);
+
+        double d = 3.4;
+        Double D;
+        D = d;
+        System.out.println(D);
+    }
+}

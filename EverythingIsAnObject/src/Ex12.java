1adb1de154a160df2eb079e39fc624cd3a84edf3
RyanDurling <ryan.p.durling@gmail.com>
2012-09-03 14:12:38 -0700

    Exercise 12

---
+++ EverythingIsAnObject/src/Ex12.java
@@ -0,0 +1,19 @@
+//: Ex12 = HelloDate form book
+
+import java.util.Date;
+
+/**  The first thinking in java example program
+ *  Displays a string and today's date.
+ *  @author Ryan Durling
+ */
+
+public class Ex12 {
+    /** Entry point to class and applications
+     *
+     * @param args array of string arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, it's: ");
+        System.out.println(new Date());
+    }
+}

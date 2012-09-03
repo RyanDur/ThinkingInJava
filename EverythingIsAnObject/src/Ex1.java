a4172f7b15c85246544ae96d2830c0ad28a359b7
RyanDurling <ryan.p.durling@gmail.com>
2012-09-03 12:07:37 -0700

    Proving default values are provided for uninitialized variables

---
+++ EverythingIsAnObject/src/Ex1.java
@@ -0,0 +1,11 @@
+public class Ex1 {
+    int i;
+    char ch;
+
+    public static void main(String[] args) {
+        Default1 default1 = new Default1();
+        System.out.println(default1.getCh());
+        System.out.println(default1.getI());
+    }
+}
+
